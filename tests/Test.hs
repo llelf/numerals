b50fcc67d39abba5629d8716bbb8d128c9ed17a0
Antonio Nikishaev <me@lelf.lu>
2015-07-30 22:19:52 +0000

    some tests

---
+++ tests/Test.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Test where
+
+import qualified Data.Text as T
+import Data.Function
+import Data.Text.Numerals
+
+testsFi :: [(Integer,T.Text)]
+testsFi = [
+ (0, "nolla"),
+ (7, "seitsemän"),
+ (11, "yksi-toista"),
+ (12, "kaksi-toista"),
+ (20, "kaksi-kymmentä"),
+ (21, "kaksi-kymmentä yksi"),
+ (22, "kaksi-kymmentä kaksi"),
+ (30, "kolme-kymmentä"),
+ (40, "neljä-kymmentä"),
+ (90, "yhdeksän-kymmentä"),
+ (100, "sata"),
+ (200, "kaksisataa"),
+ (1000, "tuhat"),
+ (2000, "kaksituhatta"),
+ (3721,  "kolme-tuhatta-seitsemän-sataa-kaksi-kymmentä-yksi"),
+ (1026234, "yksi miljoona kaksikymmentäkuusituhattakaksisataakolmekymmentäneljä")
+ ]
+
+
+norm = T.concat . T.split (`elem` " -\xad")
+
+test = all f testsFi
+    where f (n,s) = ((==)`on`norm) (spellFi n) s || error (show n)
